b2617a6979d07bf4ca7012189fc88b122f934b1c
Maxaliseur <maxime.lanceleur.ml@gmail.com>
2013-11-04 11:12:44 +0100

    first commit

---
+++ src/js/tests.js
@@ -0,0 +1,30 @@
+/**test( "hello test", function() {
+  ok( 1 == "1", "Passed!" );
+});**/
+
+/**var myString= "hello Backone.js";
+test('our first QUnit test - asserting resullts', function(){
+
+	ok(true, 'the test succeeds');
+	ok(false, 'the test not succeeds');
+
+	equal(myString, 'hello Backbone.js','The value expected is Hello Backbone');
+});**/
+
+SimpleMath = function(){
+
+};
+
+SimpleMath.prototype.getFactorial = function(number){
+
+};
+
+SimpleMath.prototype.signum = function(number){
+
+};
+
+SimpleMath.prototype.average = function(number){
+
+};
+
+
