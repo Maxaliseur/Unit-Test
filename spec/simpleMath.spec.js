9f6c7670836f251364b7d558f7fc1702334e4936
Maxaliseur <maxime.lanceleur.ml@gmail.com>
2013-11-04 13:02:33 +0100

    average

--- spec/simpleMath.spec.js
+++ spec/simpleMath.spec.js
@@ -13,11 +13,55 @@ test('calculating factorial for a positive number', function(){
 });
 
 test('calculating factorial for zero', function(){
- equal(this.simpleMath.getFactorial(0), 1, 'Factorial of zeromust equal one');
+ equal(this.simpleMath.getFactorial(0), 1, 'Factorial of zero must equal one ');
 });
 
 test('throwing an error when calculating the factorial for a negative number', function(){
  raises(function(){
-  // todo
+  this.simpleMath.getFactorial(-10);
  }, "There is no factorial for negative number");
 })
+
+module( 'signum', {
+ setup : function() {
+  // return before
+  this.simpleMath = new SimpleMath();
+ }, teardown : function(){
+  // return after
+  delete this.simpleMath;
+ },
+});
+
+test('calculating factorial for a positive number', function(){
+ equal(this.simpleMath.signum(10), 1, 'Result is 1 ');
+});
+
+test('calculating factorial for zero', function(){
+ equal(this.simpleMath.signum(0), 0, 'Result is 0 ');
+});
+
+test('throwing an error when calculating the factorial for a negative number', function(){
+ equal(this.simpleMath.signum(-10),-1, 'Result -1');
+});
+
+module( 'average', {
+ setup : function() {
+  // return before
+  this.simpleMath = new SimpleMath();
+ }, teardown : function(){
+  // return after
+  delete this.simpleMath;
+ },
+});
+
+test('calculating factorial for a positive number', function(){
+ equal(this.simpleMath.average(15,3),9, 'average ');
+});
+
+test('calculating factorial for zero', function(){
+ equal(this.simpleMath.average(0,0), 0, 'average is 0 ');
+});
+
+test('throwing an error when calculating the factorial for a negative number', function(){
+ equal(this.simpleMath.average(-10,25),7, 'Result 7');
+});
--- src/js/simpleMath.js
+++ src/js/simpleMath.js
@@ -1,15 +1,28 @@
 SimpleMath = function(){
- 
+
 };
 
 SimpleMath.prototype.getFactorial = function(number){
-
+ if(number>0){
+  return number * this.getFactorial(number-1);
+ }else if(number == 0){
+  return 1;
+ }else{
+  throw new Error("there is no factorial for a negative numbers");
+ }
 };
 
 SimpleMath.prototype.signum = function(number){
-
+ if(number>0){
+  return 1;
+ }else if(number == 0){
+  return 0;
+ }else{
+  return -1
+ }
 };
 
-SimpleMath.prototype.average = function(number1,number2){
-
+SimpleMath.prototype.average = function(number1, number2){
+	var sum = number1+number2;
+	return Math.floor(sum/2);
 };
